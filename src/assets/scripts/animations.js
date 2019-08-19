7b7941a6ceed53611f61c8984802aacd67069b7f
EvgeniiYakuba <evgenii.yakuba@saritasa.com>
2019-08-19 23:54:39 +0700

    add about page

--- src/assets/scripts/animations.js
+++ src/assets/scripts/animations.js
@@ -1,5 +1,6 @@
 import ScrollMagic from 'ScrollMagic';
 import { TimelineMax } from 'gsap';
+import _ from 'loadsh';
 import 'animation.gsap';
 
 const controller = new ScrollMagic.Controller();
@@ -60,3 +61,103 @@ $('.highlight-button').click(function() {
     parent.find('.highlight-content').slideToggle();
     $(this).toggleClass('active')
 })
+
+
+// parallax scroll
+
+let ticking = false;
+let isFirefox = (/Firefox/i.test(navigator.userAgent));
+let isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+let scrollSensitivitySetting = 25; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
+let slideDurationSetting = 1000; //Amount of time for which slide is "locked"
+let currentSlideNumber = 0;
+let totalSlideNumber = $(".background").length;
+let delta;
+
+// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+function wheelScroll(evt) {
+  if (isFirefox) {
+    //Set delta for Firefox
+    delta = evt.detail * (-120);
+  } else if (isIe) {
+    //Set delta for IE
+    delta = -evt.deltaY;
+  } else {
+    //Set delta for all other browsers
+    delta = evt.wheelDelta;
+  }
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+function touchScroll(ts, te) {
+  delta = te - ts;
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+function slideDurationTimeout(slideDuration) {
+  setTimeout(function() {
+    ticking = false;
+  }, slideDuration);
+}
+
+// ------------- ADD EVENT LISTENER ------------- //
+let mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+let ts;
+
+//window.addEventListener(mousewheelEvent, _.throttle(wheelScroll, 60), false);
+window.addEventListener(mousewheelEvent, _.throttle(wheelScroll, 60), false);
+window.addEventListener("touchstart", function(e) {
+  ts = e.touches[0].clientY;
+}, false);
+window.addEventListener("touchend", function(e) {
+  let te = e.changedTouches[0].clientY;
+  touchScroll(ts, te);
+}, false);
+
+// ------------- SLIDE MOTION ------------- //
+function nextItem() {
+  let $previousSlide = $(".background").eq(currentSlideNumber - 1);
+  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+}
+
+function previousItem() {
+  let $currentSlide = $(".background").eq(currentSlideNumber);
+  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+}
